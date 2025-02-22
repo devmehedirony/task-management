4b2f120c447123c1949a1d39b599066d470c0dab
devmehedirony <devmehedirony@gmail.com>
2025-02-22 12:42:02 +0600

    add task page done

--- src/Layouts/RootLayout.jsx
+++ src/Layouts/RootLayout.jsx
@@ -1,9 +1,11 @@
+import { Outlet } from "react-router-dom";
 import NavBar from "../Components/NavBar";
 
 const RootLayout = () => {
   return (
     <div>
       <NavBar></NavBar>
+      <Outlet></Outlet>
     </div>
   );
 };
--- src/Pages/AddTasks.jsx
+++ src/Pages/AddTasks.jsx
@@ -1,9 +1,67 @@
-
+import { useForm } from "react-hook-form";
+import { useAxios } from "../Hooks/useAxios";
+import Swal from "sweetalert2";
 
 const AddTasks = () => {
+  const axios = useAxios()
+
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm()
+
+  const onSubmit = async (data) => {
+    console.log(data);
+    const task = {
+      title: data.title,
+      description: data.description,
+      category: data.category,
+      timestamp: new Date().toLocaleString()
+    }
+
+    axios.post('/tasks', task)
+      .then(res => {
+        console.log(res.data);
+        if (res.data.insertedId) {
+          Swal.fire({
+            title: "Task",
+            text: "Added Successfully",
+            icon: "success"
+          });
+        }
+    })
+   }
+
   return (
-    <div>
-      
+    <div className="max-w-lg mx-auto p-6 bg-white rounded-xl shadow-lg">
+      <h2 className="text-xl font-semibold mb-4">ADD TASK</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="mb-4">
+          <label className="block text-sm font-medium">Task Title</label>
+          <input {...register("title", { required: true })} type="text" className="w-full p-2 border rounded-md mt-1" />
+        </div>
+        <div className="mb-4">
+          <label className="block text-sm font-medium">Description</label>
+          <input {...register("description", { required: true })} type="text" className="w-full p-2 border rounded-md mt-1" />
+        </div>
+        <div className=" mb-6">
+          <div>
+            <label className="block text-sm font-medium">Task Stage</label>
+            <select className="w-full p-2 border rounded-md mt-1 " {...register("category", { required: true })}>
+              <option value="TODO">TODO</option>
+              <option value="IN PROGRESS">PROGRESS</option>
+              <option value="DONE">DONE</option>
+            </select>
+          </div>
+        
+        </div>
+       
+        <div className="flex justify-center items-center">
+          <button type="submit" className="bg-blue-600 text-white px-4 py-2  cursor-pointer">Add Task</button>
+        </div>
+      </form>
     </div>
   );
 };
